6d53a73816697d2a84d6a8d5b430cb4cd8816def
CodeSandbox <support@codesandbox.io>
2024-11-26 08:20:34 +0000

    Sandbox snapshot commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>Hello</h3>
+      <h2>How are you ka haal ba</h2>
+    </div>
+  );
+}
